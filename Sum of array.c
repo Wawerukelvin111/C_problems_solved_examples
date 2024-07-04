ea7487a5eebf21094924086b2f0b2ccf45e90488
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-07-04 08:59:01 +0000

    problem 1 commented
    Problem 01:Sum of Array
    // Take an array of integer data type of size 10
    // Get input from user and store the values in the array and
    // finally print the sum of those integers

--- Sum of array.c
+++ Sum of array.c
@@ -3,16 +3,16 @@
 // Get input from user and store the values in the array and
 // finally print the sum of those integers
 #include<stdio.h>
- int main(){
-    int i,myarray[10],sum=0;
-    for(i=0;i<10;i++){
-        printf("\nEnter array[%d]:",i);
-        scanf("%d",&myarray[i]);
+ int main(){//the start of progran execution
+    int i,myarray[10],sum=0;//we declare the variables
+    for(i=0;i<10;i++){//loop statement for repeated user input
+        printf("\nEnter array[%d]:",i);//
+        scanf("%d",&myarray[i]);//accepts the user to input 10 integers with index from 0 - 9
     }
     for (i=0;i<10;i++){
-        sum+=myarray[i];
+        sum+=myarray[i];//this program loops through the entered arrays and adds them 
     }
-    printf("The sum of entered arrays is:%d",sum);
+    printf("The sum of entered arrays is:%d",sum);//total sum displayed at the console
 
     return 0;
 
